ce3f404e11dd3a7a8423db8680cc67718cdb3ee6
Smuggler0p <avimathur789@gmail.com>
2025-11-23 14:02:16 +0000

    feat: Added Barcode Printing

---
+++ jain_machine_tools/jain_machine_tools/doctype/barcode_printing/barcode_printing.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Praxon Technovation and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class BarcodePrinting(Document):
+	pass
